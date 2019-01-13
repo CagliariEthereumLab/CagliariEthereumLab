9703578119529ebb759f9501adc9d8fb7fdb3952
dadaista <634159+dadaista@users.noreply.github.com>
2019-01-13 16:52:14 +0100

    Update with Norman code

--- RSVP.sol
+++ RSVP.sol
@@ -1,56 +1,83 @@
+//Current version:0.4.25+commit.59dbf8f1.Emscripten.clang
 pragma solidity 0.4.25;
 
 
-contract RSVP{
-    mapping (address => bool) public bookings;
+contract RSVP {
+
     address public owner;
-    uint N=3;
-    bytes32 [] unlockCodesHashes = new bytes32[](N);
     uint RESERVATION = 0;
     uint UNLOCKING = 1;
-    uint state=RESERVATION;
-    
+    uint state;
 
+    uint public stakeholders;
+
+    mapping(address => bool) public bookings;
+    mapping(bytes32 => bool) stakes;
+
+    modifier restricted () {
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor() public{
         owner = msg.sender;
-        unlockCodesHashes[0] = 0xA2242EAD55C94C3DEB7CF2340BFEF9D5BCACA22DFE66E646745EE4371C633FC8;//pippo
-        unlockCodesHashes[1] = 0xC48B4DF565B0C96F84FEDF18F26596ED40AA9F46F11021AF7125D34D1D3ACFFE;//pluto
-        unlockCodesHashes[2] = 0xF106E246ECDAB88CB2262780F60079651AEAA6A3C8F5B1E75FC2AB0582CD3F67;//paperino
+        state = RESERVATION;
+        stakeholders = 0;
     }
-    
-    function reserveSeat() payable public{
+
+    function addCode(string _code) public restricted {
+        bytes32 hash = sha3(_code);
+        require (stakes[hash] == false); // Don't overwrite previous mappings and return false
+        stakes[hash] = true;
+    }
+
+    function verifyCode(string _code) view public restricted returns (bool) {
+        bytes32 hash = sha3(_code);
+        return stakes[hash];
+    }
+
+    //E' possibile sbloccare una puntata con qualsiasi codice
+    function unlockStake(string _code) public {
+        bytes32 hash = sha3(_code);
+
+        require(state == UNLOCKING);
+        require(bookings[msg.sender] == true);
+        require(stakes[hash] == true);
+
+        stakes[hash] = false;
+        bookings[msg.sender] == false;
+        stakeholders --;
+        msg.sender.transfer(0.03 ether);
+
+    }
+
+    //E' possibile fare una puntata anche se non sono stati inseriti ancora codici
+    function reserveSeat() payable public {
         require(state == RESERVATION);
         require(msg.value == 0.03 ether);
         require(bookings[msg.sender] == false);
         bookings[msg.sender] = true;
-    } 
-    
-    function () payable public{
+        stakeholders ++;
+    }
+
+    function() payable public {
         reserveSeat();
     }
-    
-    function unlockStake(string code) public {
-        require(state == UNLOCKING);
-        require(bookings[msg.sender] == true);
-        for(uint i=0;i<N;i++){
-            if(sha256(code) == unlockCodesHashes[i]) {
-                unlockCodesHashes[i]=0;
-                msg.sender.transfer(0.03 ether);
-            }
-        }
-        
-        
-    }
-    
-    function stopRSVP(){
-        require(msg.sender == owner);
+
+    function stopRSVP() public restricted {
         state = UNLOCKING;
     }
-    
-    function close() public{
-        require(msg.sender == owner);
+
+    function close() public restricted {
         selfdestruct(owner);
     }
-    
+
 }
+
+
+
+
+
+
+
+
